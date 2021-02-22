b04d81fec2806e2f2b7054a667be1fc8f4a9050d
Tinh Nguyen <tinh.nguyen@knorex.com>
2021-02-22 22:09:11 +0700

    update

--- dataset/preprocessing.py
+++ dataset/preprocessing.py
@@ -38,10 +38,11 @@ def generate_data_description(save_dir):
     dataset.attr_name = [pa100k_data['attributes'][i][0][0] for i in range(26)]
 
     dataset.partition = EasyDict()
-    dataset.partition.train = np.arange(0, 90000)  # np.array(range(80000))
+    dataset.partition.train = np.arange(0, 89984)  # np.array(range(80000))
     # dataset.partition.val = np.arange(90000, 100000)  # np.array(range(80000, 90000))
     dataset.partition.test = np.arange(90000, 100000)  # np.array(range(90000, 100000))
     dataset.partition.trainval = np.arange(0, 90000)  # np.array(range(90000))
+    # print(train_image_name)
 
     with open(os.path.join(save_dir, 'dataset.pkl'), 'wb+') as f:
         pickle.dump(dataset, f)
